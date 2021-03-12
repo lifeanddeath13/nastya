4fe360f608361021086449c06c5ab26f262a9a67
lifeanddeath13 <80498956+lifeanddeath13@users.noreply.github.com>
2021-03-12 09:22:41 +0600

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 	<title></title>
 </head>
 <body>
-<h1>hello world!</h1>
+<body style="background-color:powderblue;">
+<h1>Anastasiia Dzhorobaeva
+17 y.o.
+Ala Too Vocational School</h1>
+<p> My hobby is swimming.I engaged in it for 8 years.I started my "career" in Osh,the city where i was born and continued in Bishkek,where i live now. I am third-year student at college .
+	</p>
+
 </body>
 </html>
