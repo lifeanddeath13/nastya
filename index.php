14bb52fe42d78ec0d220d31bb114c461a3767af0
lifeanddeath13 <80498956+lifeanddeath13@users.noreply.github.com>
2021-03-12 10:00:38 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 <html>
 <head>
 	<title></title>
-</head>
-<body>
+</head><h1 style="color:#7d32a8;"font-size:300%;">My profile</h1>
 <body style="background-color:powderblue;">
-<h1>Anastasiia Dzhorobaeva</h1>
-	<h2>17 y.o.</h2>
-	<h3>Ala Too Vocational School</h3>
-<p> My hobby is swimming.I engaged in it for 8 years.I started my "career" in Osh,the city where i was born and continued in Bishkek,where i live now. I am third-year student at college .
+<img src="photo.jpg width="224" height="224"> 
+<h2 style="color:Tomato;">Anastasiia Dzhorobaeva
+<br>17 y.o.
+<br>Ala Too Vocational School</h2>
+<p> My hobby is swimming.I engaged in it for 8 years.I started my "career" in Osh,the city where i was born and continued in Bishkek,where i live now.
 	</p>
-	 <img src="photo.jpg" alt="Girl in a jacket"> 
 
 </body>
 </html>
