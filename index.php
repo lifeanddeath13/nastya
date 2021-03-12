5be4129b567ff74db9614e7381bc2bea672cf089
lifeanddeath13 <80498956+lifeanddeath13@users.noreply.github.com>
2021-03-12 09:55:32 +0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 	<h3>Ala Too Vocational School</h3>
 <p> My hobby is swimming.I engaged in it for 8 years.I started my "career" in Osh,the city where i was born and continued in Bishkek,where i live now. I am third-year student at college .
 	</p>
+	 <img src="photo.jpg" alt="Girl in a jacket"> 
 
 </body>
 </html>
