676a0ca218b40fcd1ba4d900417adcb9d442895a
lifeanddeath13 <80498956+lifeanddeath13@users.noreply.github.com>
2021-03-20 21:08:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,22 @@
 Login: <input type="text" name="login"><br><br>
 Password: <input type="text" name="password"><br><br>
 <button type="Submit" value="Submit">Submit</button>
-<button type="Reser" value="Reset">Clear</button>
-</form>
-<php
+<button type="Reset" value="Reset">Clear</button>
 
-
-
+<script>
+	
+	    document.forms[0].onsubmit = function(e){
+        let login = document.getElementById('fname').value;
+        let password = document.getElementById('fpass').value;
+ 
+        if ( login === "Anastasiya" && password === "123456789") 
+            alert("Welcome");
+        else {
+            e.preventDefault();
+            alert("Invalid password or login");
+        }
+    };
+    
+	</script>
 </body>
 </html>
