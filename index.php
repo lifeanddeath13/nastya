cbd7a482cb09a5f9e23ee244faac9b8e80ae0250
lifeanddeath13 <80498956+lifeanddeath13@users.noreply.github.com>
2021-03-20 21:45:51 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 <body>
 
 <form action="main.php" method="get">
-Login: <input type="text" name="login"><br><br>
-Password: <input type="text" name="password"><br><br>
+<div class="first_name">
+                    <label for="fname">First name</label>
+                    <input type="text" placeholder="name" class="fname" id="fname" name="fname"><br>
+                </div>
+                <div class="pass_wrap">
+                    <label for="password" class="pass_title">Password</label>
+                    <input type="password" placeholder="password" class="fpass" id="fpass" name="password"><br>
+                </div>
 <button type="Submit" value="Submit">Submit</button>
 <button type="Reset" value="Reset">Clear</button>
+</form>
 
 <script>
 	
