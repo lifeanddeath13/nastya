e4c61db86f94ccc4060516490a89457b0e3c1d4c
lifeanddeath13 <80498956+lifeanddeath13@users.noreply.github.com>
2021-03-26 10:24:46 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,17 +3,16 @@
 
 <form action="main.php" method="get">
 <div class="first_name">
-                    <label for="fname">First name</label>
+                    <label for="fname" align="center">Username</label>
                     <input type="text" placeholder="name" class="fname" id="fname" name="fname"><br>
                 </div>
                 <div class="pass_wrap">
-                    <label for="password" class="pass_title">Password</label>
+                 <br>   <label for="password" align="center" class="pass_title">Password</label>
                     <input type="password" placeholder="password" class="fpass" id="fpass" name="password"><br>
-                </div>
-<button type="Submit" value="Submit">Submit</button>
+                </div><br>
+<button type="Submit"value="Submit">Submit</button>
 <button type="Reset" value="Reset">Clear</button>
 </form>
-
 <script>
 	
 	  document.forms[0].onsubmit = function(e){
